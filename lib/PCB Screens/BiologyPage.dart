e39bd8a527874196f03a3bdd4a7966b88fe3dc99
Reshmika Sreenath Nambiar <64072982+Reshmika-Nambiar@users.noreply.github.com>
2021-04-08 01:19:56 +0530

    Added PCB Pages

---
+++ lib/PCB Screens/BiologyPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BiologyPage extends StatefulWidget {
+  @override
+  _BiologyPageState createState() => _BiologyPageState();
+}
+
+class _BiologyPageState extends State<BiologyPage> {
+  @override
+  Widget build(BuildContext context) {
+    print("Biology");
+    return Container();
+  }
+}
---
+++ lib/PCB Screens/ChemistryPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChemistryPage extends StatefulWidget {
+  @override
+  _ChemistryPageState createState() => _ChemistryPageState();
+}
+
+class _ChemistryPageState extends State<ChemistryPage> {
+  @override
+  Widget build(BuildContext context) {
+    print("Chemistry called");
+    return Container();
+  }
+}
