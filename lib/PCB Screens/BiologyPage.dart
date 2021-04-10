fbbe2d8351121699440f9317dbd077ee017aaf7d
Reshmika Sreenath Nambiar <64072982+Reshmika-Nambiar@users.noreply.github.com>
2021-04-10 13:59:58 +0530

    Updated PCB Screens

--- lib/PCB Screens/BiologyPage.dart
+++ lib/PCB Screens/BiologyPage.dart
@@ -1,14 +1,89 @@
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class BiologyPage extends StatefulWidget {
+import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/chapter.dart';
+import 'package:flutter_application_1/constants.dart';
+
+final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
+class Biology extends StatefulWidget {
   @override
-  _BiologyPageState createState() => _BiologyPageState();
+  _BiologyState createState() => _BiologyState();
 }
 
-class _BiologyPageState extends State<BiologyPage> {
+class _BiologyState extends State<Biology> {
   @override
   Widget build(BuildContext context) {
-    print("Biology");
-    return Container();
+    print("Biology called");
+    return Container(
+        child: StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection("BChapters").snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return spinkit;
+        }
+        return ListView.builder(
+            itemCount: snapshot.data.docs.length,
+            itemBuilder: (context, index) {
+              DocumentSnapshot sub = snapshot.data.docs[index];
+              return Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChapterView(sub.id, "BChapters"),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    elevation: 10.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(12.0),
+                    ),
+                    child: Container(
+                      height: 430,
+                      width: 450,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Wrap(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Center(
+                                child: Text(sub['name'],
+                                    style: TextStyle(
+                                      fontSize: 19,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w500,
+                                    )),
+                              ),
+                            ),
+
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Image.network(sub['img']),
+                            ),
+                            /*FAProgressBar(
+                              currentValue: sub['percentage'],
+                              maxValue: 100,
+                              direction: Axis.horizontal,
+                              backgroundColor: Colors.white,
+                              progressColor: Colors.teal,
+                              size: 10.0,
+                            )*/
+                            // textAlign: TextAlign.center,
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            });
+      },
+    ));
   }
 }
--- lib/PCB Screens/PhysicsPage.dart
+++ lib/PCB Screens/PhysicsPage.dart
@@ -1,14 +1,89 @@
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class PhysicsPage extends StatefulWidget {
+import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/chapter.dart';
+import 'package:flutter_application_1/constants.dart';
+
+final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
+class Physics extends StatefulWidget {
   @override
-  _PhysicsPageState createState() => _PhysicsPageState();
+  _PhysicsState createState() => _PhysicsState();
 }
 
-class _PhysicsPageState extends State<PhysicsPage> {
+class _PhysicsState extends State<Physics> {
   @override
   Widget build(BuildContext context) {
     print("Physics called");
-    return Container();
+    return Container(
+        child: StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection("PChapters").snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return spinkit;
+        }
+        return ListView.builder(
+            itemCount: snapshot.data.docs.length,
+            itemBuilder: (context, index) {
+              DocumentSnapshot sub = snapshot.data.docs[index];
+              return Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChapterView(sub.id, "PChapters"),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    elevation: 10.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(12.0),
+                    ),
+                    child: Container(
+                      height: 430,
+                      width: 450,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Wrap(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Center(
+                                child: Text(sub['name'],
+                                    style: TextStyle(
+                                      fontSize: 19,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w500,
+                                    )),
+                              ),
+                            ),
+
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Image.network(sub['img']),
+                            ),
+                            /*FAProgressBar(
+                              currentValue: sub['percentage'],
+                              maxValue: 100,
+                              direction: Axis.horizontal,
+                              backgroundColor: Colors.white,
+                              progressColor: Colors.teal,
+                              size: 10.0,
+                            )*/
+                            // textAlign: TextAlign.center,
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            });
+      },
+    ));
   }
 }
