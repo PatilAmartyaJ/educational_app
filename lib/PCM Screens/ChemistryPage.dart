940545303d324effeb9eefde6c819bc3be5886df
Prachiti Barge <prachitibarge01@gmail.com>
2021-05-10 13:19:08 +0530

    Added PCM Screens

---
+++ lib/PCM Screens/ChemistryPage.dart
@@ -0,0 +1,91 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:educational_app/ChapterView.dart';
+import 'package:educational_app/constants.dart';
+
+final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
+class ChemistryPage extends StatefulWidget {
+  @override
+  _ChemistryPageState createState() => _ChemistryPageState();
+}
+
+class _ChemistryPageState extends State<ChemistryPage> {
+  @override
+  Widget build(BuildContext context) {
+    print("Chemistry called");
+    return Container(
+        child: StreamBuilder<QuerySnapshot>(
+          stream:
+          FirebaseFirestore.instance.collection("CChapters").snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return spinkit;
+            }
+            return ListView.builder(
+                itemCount: snapshot.data.docs.length,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot sub = snapshot.data.docs[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                ChapterView(sub.id, "CChapters"),
+                          ),
+                        );
+                      },
+                      child: Card(
+                        elevation: 10.0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(12.0),
+                        ),
+                        child: Container(
+                          height: 430,
+                          width: 450,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Wrap(
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(12.0),
+                                  child: Center(
+                                    child: Text(sub['name'],
+                                        style: TextStyle(
+                                          fontSize: 19,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w500,
+                                        )),
+                                  ),
+                                ),
+
+                                Padding(
+                                  padding: const EdgeInsets.all(12.0),
+                                  child: Image.network(sub['img']),
+                                ),
+                                FAProgressBar(
+                                  currentValue: sub['percentage'],
+                                  maxValue: 100,
+                                  direction: Axis.horizontal,
+                                  backgroundColor: Colors.white,
+                                  progressColor: Colors.teal,
+                                  size: 10.0,
+                                )
+                                // textAlign: TextAlign.center,
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                });
+          },
+        ));
+  }
+}
---
+++ lib/PCM Screens/PCMHome.dart
@@ -0,0 +1,104 @@
+// import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_core/firebase_core.dart';
+//
+// class JeePCM extends StatefulWidget {
+//   @override
+//   _JeePCMState createState() => _JeePCMState();
+// }
+//
+// class _JeePCMState extends State<JeePCM> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:educational_app/PCM%20Screens/PhysicsPage.dart';
+import 'package:educational_app/PCM%20Screens/ChemistryPage.dart';
+import 'package:educational_app/PCM%20Screens/MathsPage.dart';
+import 'package:educational_app/SearchEngine.dart';
+import 'package:spincircle_bottom_bar/modals.dart';
+import 'package:spincircle_bottom_bar/spincircle_bottom_bar.dart';
+
+
+class PCMHome extends StatelessWidget {
+  List<String> l = [
+    "Physics",
+    "Chemistry",
+    "Mathematics",
+  ];
+  static String id = "PCMHome";
+  final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+  final Color moreDark = Color.fromARGB(255, 20, 32, 50);
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: l.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("PCM"),
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          centerTitle: true,
+          bottom: TabBar(
+              isScrollable: true,
+              unselectedLabelColor: Colors.white.withOpacity(0.3),
+              tabs: [
+                for (int i = 0; i < l.length; i++)
+                  Tab(
+                    child: Text(l[i]),
+                  )
+              ]),
+        ),
+        body: SpinCircleBottomBarHolder(
+          bottomNavigationBar: SCBottomBarDetails(
+              iconTheme: IconThemeData(color: Colors.white),
+              activeIconTheme: IconThemeData(color: Colors.teal),
+              titleStyle: TextStyle(color: Colors.white, fontSize: 10),
+              activeTitleStyle:
+              TextStyle(color: Colors.teal, fontWeight: FontWeight.bold),
+              circleColors: [Colors.white, Colors.orange, Colors.redAccent],
+              actionButtonDetails: SCActionButtonDetails(
+                  color: Colors.red,
+                  icon: Icon(Icons.expand_less),
+                  elevation: 0.0),
+              backgroundColor: moreDark,
+              items: <SCBottomBarItem>[
+                SCBottomBarItem(
+                    icon: Icons.home, onPressed: () {}, title: "Home"),
+                SCBottomBarItem(
+                    icon: Icons.search,
+                    onPressed: () {
+                      Navigator.pushNamed(context, SearchEngine.id);
+                    },
+                    title: "Search"),
+                SCBottomBarItem(
+                    icon: Icons.assignment,
+                    onPressed: () {},
+                    title: "Take Test"),
+                SCBottomBarItem(
+                    icon: Icons.history, onPressed: () {}, title: "History")
+              ],
+              circleItems: <SCItem>[
+                SCItem(icon: Icon(Icons.search), onPressed: () {}),
+              ]),
+          child: Container(
+              color: Colors.grey.withAlpha(100),
+              child: TabBarView(
+                children: <Widget>[
+                  PhysicsPage(),
+                  ChemistryPage(),
+                  MathsPage(),
+                ],
+              )),
+        ),
+      ),
+    );
+  }
+}
