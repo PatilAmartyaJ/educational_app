e40eff5dfe0919a781bd5921396d7415b9b0655e
sarah-nisar <sarahtisekar@gmail.com>
2021-05-21 18:08:02 +0530

    Geography Screen

--- lib/NTSE Screens/GeographyScreens/NG1.dart
+++ lib/NTSE Screens/GeographyScreens/NG1.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:flutter/material.dart';
@@ -24,6 +26,10 @@ class _NG1State extends State<NG1> {
             itemCount: snapshot.data.docs.length,
             itemBuilder: (context, index) {
               DocumentSnapshot sub = snapshot.data.docs[index];
+              int index2 = sub['img'].length;
+              print(index2);
+              bool _first = true;
+              final random = Random();
               return Padding(
                 padding: const EdgeInsets.all(12.0),
                 child: GestureDetector(
@@ -32,7 +38,7 @@ class _NG1State extends State<NG1> {
                       context,
                       MaterialPageRoute(
                         builder: (context) =>
-                            ChapterView(sub.id, "10Th NGeography"),
+                            ChapterView(sub.id, "10Th NGeography", index2),
                       ),
                     );
                   },
@@ -63,7 +69,18 @@ class _NG1State extends State<NG1> {
                                 padding: const EdgeInsets.all(12.0),
                                 child: ClipRRect(
                                     borderRadius: BorderRadius.circular(12.0),
-                                    child: Image.network(sub['img']))),
+                                    child: AnimatedCrossFade(
+                                      duration: Duration(seconds: 6),
+                                      reverseDuration: Duration(seconds: 6),
+                                      firstChild: Image.network(
+                                          sub['img'][random.nextInt(index2)]),
+                                      crossFadeState: _first
+                                          ? CrossFadeState.showFirst
+                                          : CrossFadeState.showSecond,
+                                      secondChild: Image.network(
+                                          sub['img'][random.nextInt(index2)]),
+                                    ))),
+
                             // textAlign: TextAlign.center,
                           ],
                         ),
